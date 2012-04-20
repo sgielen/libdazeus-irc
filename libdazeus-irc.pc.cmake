a8f6a8134dbdfe6c5968191f2bf00ff436ae0646
Sjors Gielen <sjors@sjorsgielen.nl>
2012-04-20 04:52:53 +0200

    Add a pkgconfig file for libdazeus-irc

---
+++ libdazeus-irc.pc.cmake
@@ -0,0 +1,5 @@
+Name: ${PROJECT_NAME}
+Description: A C++ wrapper around the libircclient library
+Version: ${LIBDAZEUS_IRC_VERSION}
+Libs: -L${LIB_INSTALL_DIR} -ldazeus-irc
+Cflags: -I${INCLUDE_INSTALL_DIR}
